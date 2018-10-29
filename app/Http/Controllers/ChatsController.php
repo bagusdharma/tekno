f274500900f738a0d13fc93ec71d24dd50db45f7
bagusdharma <bagus.dharma@gmail.com>
2018-10-29 18:03:23 +0700

    re-commit

---
+++ app/Http/Controllers/ChatsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Message;
+use Illuminate\Support\Facades\Auth;
+class ChatsController extends Controller
+{
+ 	public function __construct ()
+ 	{
+ 		$this->middleware('auth');
+ 	}   
+
+ 	public function index()
+ 	{
+ 		return view('chat');
+ 	}
+
+ 	// Fetch Chat 
+ 	public function fetchMessage()
+ 	{
+ 		return Message::with('user')->get();
+ 	}
+
+ 	// send chat to db
+ 	public function sendMessage(Request $request)
+ 	{
+ 		$user = Auth::user();
+ 		$message=$user->message()->create([
+ 			'message' => $request->input('message')
+ 		]);
+
+ 		return ['status' => 'Message Sent'];
+ 	}
+}
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+    <strong>&copy; 2018 SBY-PC</strong>
+</footer>
