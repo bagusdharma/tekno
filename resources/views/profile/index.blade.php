6a77798e1f0113779b849e79a0a2c2b19f72b61e
bagusdharma <bagus.dharma@gmail.com>
2018-11-03 21:27:49 +0700

    update profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,4 +1,107 @@
 @extends ('master')
 
 @section('content')
+    <div class="row">
+        <div class="col-md-12">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.5671230573266!2d112.79569131432098!3d-7.289990673675351!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fa6e109e0401%3A0xec88edc62cd12dd!2sPrintKu+Digital+Printing!5e0!3m2!1sid!2sid!4v1541240507854" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen>
+                </iframe>
+           </div>
+        </div>
+    </div>
+ 
+   <div class="row">
+        <div class="col-md-3">
+            <div class="card" style="width:350px">
+                <img class="card-img-top" src="/img/print1.jpg" alt="Card image">
+                <div class="card-body">
+                   <h3 class="card-title"><strong>Andy Printing</strong></h3>
+                       <p class="card-text">Print modern yang berlokasi di Keputih, menawarkan harga print yang pas di kantong mahasiswa.
+                       </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+                <div id="demo" class="carousel slide" data-ride="carousel">
+
+                <!-- Indicators -->
+                <ul class="carousel-indicators">
+                    <li data-target="" data-slide-to="0" class="active"></li>
+                    <li data-target="" data-slide-to="1"></li>
+                    <!-- <li data-target="" data-slide-to="2"></li> -->
+                </ul>
+                
+                <!-- The slideshow -->
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="/img/slide1.jpg" alt="" width="100%" height="300px">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="/img/slide2.jpg" alt="" width="100%" height="300px">
+                    </div>
+                </div>
+                
+                <!-- Left and right controls -->
+                <a class="carousel-control-prev" href="" data-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                </a>
+                <a class="carousel-control-next" href="" data-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                </a>
+                </div>
+        </div>
+        
+        <div class="col-md-3">
+            <div class="card" style="width:350px">
+                <div class="card-body">
+                   <h3 class="card-title"><strong>Review</strong></h3>
+                   <hr>
+                       <p class="card-text"><i class="fa fa-star"></i>Termurah
+                       <br><i class="fa fa-star"></i>Pelayanan sangat cepat
+                </div>
+            </div>
+        </div>            
+    </div>
+
+
+     <div class="row">
+        <div class="col-md-3">
+        </div>
+
+        <div class="col-md-6">
+            <table class="table table-bordered table-responsive-sm table-hover">
+            <thead>
+                <tr>
+                <th>Ukuran</th>
+                <th>Berwarna</th>
+                <th>Hitam-Putih</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th>A1</th>
+                <td>Rp 3500,-</td>
+                <td>Rp 2500,-</td>
+                </tr>
+                <tr>
+                <th>A2</th>
+                <td>Rp 2000,-</td>
+                <td>Rp 1500,-</td>
+                </tr>
+                <tr>
+                <th>A3</th>
+                <td>Rp 1000,-</td>
+                <td>Rp 750,-</td>
+                </tr>
+                <tr>
+                <th>A4</th>
+                <td>Rp 500,-</td>
+                <td>Rp 300,-</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        
+        <div class="col-md-3">
+        </div>
 @endsection
