81c7cdb9544331eb822d7b353d7388ad0ef7f097
Fajar Maulana Firdaus <sosispanggang.15@gmail.com>
2018-12-04 09:51:41 +0700

    Chat udah, static

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Chat;
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('chat.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Chat  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function show($chat)
+    {
+        // $current = \Auth::user();
+        $chats = Chat::all()->toArray();
+        return view('chat.viewchat')->with('chats', $chats);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Chat  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Chat $chat)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Chat  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Chat $chat)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Chat  $chat
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Chat $chat)
+    {
+        //
+    }
+}
